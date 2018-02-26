6c59d2e443cd11077dc2e722097b0a23e7bcf5e1
Dheerajdas1992 <dheeraj.das@lxisoft.com>
2018-02-26 17:28:59 +0530

    First commit

---
+++ model/Product.java
@@ -0,0 +1,23 @@
+package com.yourname.modulename ;
+
+
+// @TODO Add proper java doc comments
+
+public class Product extends Part 
+{
+
+  private string name = null ;
+  private String description = null;
+  private TrueOrFalse display = null;
+  private 1ong id = null;
+
+
+
+  private doub1e maximumStockLevel = null;
+  private String name = null;
+  private Double reOrderLevel = null;
+  private String reference = null;
+  private String searchkey = null;
+  private List<Product> parts = new ArrayList<Product>();
+
+}
