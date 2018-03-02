c673ad056674cb53929ee4cde35141e8a6833333
VyshnavKR <vyshnavkr@gmail.com>
2018-03-02 15:21:33 +0530

    first commit

---
+++ com/vyshnav/hackathon/model/Customer.java
@@ -0,0 +1,24 @@
+package com.vyshnav.hackathon;
+
+public class Customer {
+
+	private String name;
+	private int cashInHand;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName() {
+		this.name = name;
+	}
+	
+	public int getCashInHand() {
+		return cashInHand;
+	}
+	
+	public void setCashInHand(int cashInHand) {
+		this.cashInHand = cashInHand;
+	}
+
+}
---
+++ com/vyshnav/hackathon/model/Inventory.java
@@ -0,0 +1,35 @@
+package com.vyshnav.hackathon;
+import java.util.ArrayList;
+import java.util.List;
+
+// @TODO Add proper java doc comments
+
+public Class Inventory {
+	
+	public Inventory(int id, String name, double maximumStockLevel, double reOrderLevel, List<Product> productList){
+		// code later
+	}
+
+  private int id;
+  private String name;
+  private double maximumStockLevel;
+  private double reOrderLevel;
+  private List<Product> productList = new ArrayList<Product>();
+
+
+
+  //private String description = null;
+
+  //private TrueOrFalse display = null;
+
+  //private String name = null;
+
+
+  //private String reference = null;
+
+  //private String searchkey = null;
+
+
+
+
+}
